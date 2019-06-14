2c1018aee332759aa405f6009dfa794f76d5dd2d
huuck <hookgab@gmail.com>
2019-06-14 10:34:06 +0000

    fixed log path creation and removed default folders

--- adbhoney/outputs/output_log.py
+++ adbhoney/outputs/output_log.py
@@ -18,20 +18,20 @@ class Output(object):
         LOG_LEVEL = levels[CONFIG.get('output_log', 'log_level').lower()]
         FORMAT = '%(asctime)s - %(thread)s - %(name)s - %(levelname)s - %(message)s'
         FORMATTER = logging.Formatter(FORMAT)
-
         logging.basicConfig(format=FORMAT)
         self.log_file = os.path.join(BASE, FILENAME)
         self.logger = logging.getLogger('ADBHoneypot')
         self.logger.setLevel(LOG_LEVEL)
-        FILE_HANDLER = logging.FileHandler(self.log_file)
-        FILE_HANDLER.setFormatter(FORMATTER)
-        FILE_HANDLER.setLevel(LOG_LEVEL)
-        self.logger.addHandler(FILE_HANDLER)
 
         if not os.path.exists(BASE):
             os.makedirs(BASE)
         if not os.path.exists(self.log_file):
             open(self.log_file, 'w').close()
+
+        FILE_HANDLER = logging.FileHandler(self.log_file)
+        FILE_HANDLER.setFormatter(FORMATTER)
+        FILE_HANDLER.setLevel(LOG_LEVEL)
+        self.logger.addHandler(FILE_HANDLER)
 
     def write(self, message, level=logging.INFO):
         if level is logging.DEBUG:
